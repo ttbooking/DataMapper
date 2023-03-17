9ba11c3978fb00d1392ff3ea0047de5f9af61f33
Ромашов Андрей <a.romashov@ttbooking.ru>
2023-03-17 15:56:09 +0200

    Расстановка комментариев, переработка логики назначения

--- src/Elements/DataMapper.php
+++ src/Elements/DataMapper.php
@@ -25,7 +25,7 @@ abstract class DataMapper implements Mappable, \JsonSerializable
 		}
 
 		if (!is_array($value)) {
-			throw new UnsupportedDataException('provided type is not supported for map');
+			throw new UnsupportedDataException('Переданное значение не может быть обработано');
 		}
 
 		$instance = new static();
@@ -49,30 +49,52 @@ abstract class DataMapper implements Mappable, \JsonSerializable
 					$instance->{$propertyInfo->name} = $propertyValue;
 				} else {
 					$filled = false;
+
+					/**
+					 * В первую очередь смотрим аналогичный тип и тогда устанавливаем значение
+					 */
 					foreach ($propertyInfo->propertyTypes as $propertyType) {
-						if (in_array($propertyValueType, [
-							'boolean', 'bool',
-							'integer', 'int',
-							'float', 'double',
-							'string'
-						])) {
-							$instance->{$propertyInfo->name} = $propertyValue;
-							$filled = true;
-							break;
-						} else if ($propertyType === 'array') {
-							$instance->{$propertyInfo->name} = (array)$propertyValue;
-							$filled = true;
-							break;
-						} else if (is_a($propertyType, Mappable::class, true)) {
-							$instance->{$propertyInfo->name} = $propertyType::map($propertyValue);
-							$filled = true;
-							break;
-						} else if ($propertyValue instanceof $propertyType) {
+						if ($propertyValue instanceof $propertyType) {
 							$instance->{$propertyInfo->name} = $propertyValue;
 							$filled = true;
 							break;
 						}
 					}
+
+					/**
+					 * Во вторую очередь смотрим на тип и пытаемся угадать, что нужно сделать
+					 */
+					if (!$filled) {
+						$simpleTypes = ['boolean', 'bool', 'integer', 'int', 'float', 'double', 'string'];
+						foreach ($propertyInfo->propertyTypes as $propertyType) {
+							/**
+							 * В случае если тип значения простой и тип поля простой, то однозначно можем установить
+							 */
+							if (in_array($propertyValueType, $simpleTypes) && in_array($propertyType, $simpleTypes)) {
+								$instance->{$propertyInfo->name} = $propertyValue;
+								$filled = true;
+								break;
+							} else if ($propertyType === 'array') {
+								/**
+								 * Если тип свойства массив, то установим преобразовав к массиву
+								 */
+								$instance->{$propertyInfo->name} = ArraySerializer::toArray($propertyValue);
+								$filled = true;
+								break;
+							} else if (is_a($propertyType, Mappable::class, true)) {
+								/**
+								 * Если тип наследник, то делегируем преобразование в него
+								 */
+								$instance->{$propertyInfo->name} = $propertyType::map($propertyValue);
+								$filled = true;
+								break;
+							}
+						}
+					}
+
+					/**
+					 * В последнюю очередь пробуем получить экземпляр типа, передав значение в конструктор
+					 */
 					if (!$filled) {
 						foreach ($propertyInfo->propertyTypes as $type) {
 							if (class_exists($type)) {
